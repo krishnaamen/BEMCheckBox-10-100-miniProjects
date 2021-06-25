7c0fd27f1bcf18a0cffdfc6eb3df7501eec1e764
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-06-25 07:43:20 +0200

    first commit

--- BEMCheckBox/ViewController.swift
+++ BEMCheckBox/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,BEMCheckBoxDelegate {
+    @IBOutlet weak var checkBox1: BEMCheckBox!
+    @IBOutlet weak var checkBox2: BEMCheckBox!
+    
+    @IBOutlet weak var checkBox3: BEMCheckBox!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkBox1.delegate = self
+        checkBox2.delegate = self
+        checkBox3.delegate = self
+    }
+    func didTap(_ checkBox: BEMCheckBox) {
+        if checkBox.tag == 1{
+            self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        else if checkBox.tag == 2{
+            self.view.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        }
+        else if checkBox.tag == 3{
+            self.view.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        }
     }
 
 
